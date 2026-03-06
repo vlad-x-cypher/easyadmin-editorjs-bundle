ae458143929a1b081022bd0dcc634619af353da9
Vlad Kurevlev <55359018+vlad-x-cypher@users.noreply.github.com>
2026-03-06 10:31:02 +0300

    fix: service di

--- config/services.php
+++ config/services.php
@@ -3,14 +3,15 @@
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Symfony\Component\String\Slugger\SluggerInterface;
+use VladX\EasyadminEditorjsBundle\Parser\ParserInterface;
 use VladX\EasyadminEditorjsBundle\Twig\Components\Paragraph;
 use VladX\EasyadminEditorjsBundle\Twig\Components\Header;
 use VladX\EasyadminEditorjsBundle\Parser\Parser;
 
 return static function (ContainerConfigurator $container) {
     $container->services()
-        ->set(Parser::class)
-            ->factory([null, 'createParserInstance'])
+        ->set(ParserInterface::class)
+            ->factory([Parser::class, 'createParserInstance'])
             ->args([service(SluggerInterface::class)->nullOnInvalid()])
 
         ->set(Paragraph::class)
